1b7c3974affc354b0b007a9dfa5e1b3ebbcdc66b
Marcos Cevallos <mjcevallos007@gmail.com>
2024-02-16 12:57:55 -0500

    Add new config without errors

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+  { "talha-akram/noctis.nvim", priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "noctis_obscuro",
+    },
+  },
+}
