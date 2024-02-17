2c039c688ca05599d7281e526a75844951c39be6
Marcos Cevallos <mjcevallos007@gmail.com>
2024-02-17 10:39:17 -0500

    some more keybiding changed

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- My Basic ones
+keymap.set("i", "<C-BS>", "<C-W>", opts) -- delete whole word with delete
+keymap.set("i", "<C-S-Left", "<ESC>biw", opts)
+keymap.set("i", "<C-S-Right", "<ESC>eiw", opts)
+-- --
